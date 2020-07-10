afaf26d7c4b967d26e78aff4737c35d50ebcc1fd
Aero51 <nsrdoc@yahoo.com>
2020-07-10 18:51:42 +0200

    increased retrofit timeouts

--- src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitInstance.java
+++ src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitInstance.java
@@ -37,8 +37,8 @@ public class RetrofitInstance {
 			// addInterceptor(REWRITE_CONTENT_LENGTH_INTERCEPTOR)
 			// builder.connectTimeout(30, TimeUnit.SECONDS);
 			// builder.readTimeout(30, TimeUnit.SECONDS);
-			OkHttpClient okHttpClient = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS)
-					.readTimeout(10, TimeUnit.SECONDS).callTimeout(25, TimeUnit.SECONDS).proxy(proxy)
+			OkHttpClient okHttpClient = new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS)
+					.readTimeout(15, TimeUnit.SECONDS).callTimeout(30, TimeUnit.SECONDS).proxy(proxy)
 					.retryOnConnectionFailure(false).addInterceptor(loggingInterceptor)
 					.addNetworkInterceptor(new Interceptor() {
 						@NotNull
