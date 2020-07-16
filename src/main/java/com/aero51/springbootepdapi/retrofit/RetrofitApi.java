40a7a78ed064b7f7c76b92e6d60822367101bcd8
Aero51 <nsrdoc@yahoo.com>
2020-07-16 23:40:01 +0200

    added accept encoding identity header

--- src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
+++ src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
@@ -14,7 +14,9 @@ public interface RetrofitApi {
 	// @Headers({ "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)
 	// AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36" })
 	@Streaming
-	@Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
+	@Headers({
+			"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+			"Accept-Encoding: identity" })
 	@GET("/")
 	Call<Tv> getEpg();
 
