9dc5379f8a1dca2b72324c969ccb7b2e343ab215
Aero51 <nsrdoc@yahoo.com>
2020-07-17 20:33:35 +0200

    changed epg link from http to https

--- src/main/java/com/aero51/springbootepdapi/InitialLoadApplicationRunner.java
+++ src/main/java/com/aero51/springbootepdapi/InitialLoadApplicationRunner.java
@@ -124,13 +124,13 @@ public class InitialLoadApplicationRunner implements ApplicationRunner {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
+/*
 				if (pubProxyFailcount < 51) {
 					fetchNewPubProxy();
 				} else {
 					fetchNewGimmeProxy();
 				}
-
+*/
 			}
 		});
 
