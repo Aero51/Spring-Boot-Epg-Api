5ef9435393a384af04944cdab60819da080acf27
Aero51 <nsrdoc@yahoo.com>
2020-07-22 16:50:15 +0200

    accept identity

--- src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
+++ src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
@@ -16,8 +16,7 @@ public interface RetrofitApi {
 	// AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36" })
 	// "Content-Type: application/gzip",
 	@Streaming
-	@Headers({ "Connection: close", "Accept-Encoding: identity",
-			"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36" })
+	@Headers({ "Connection: close", "Accept-Encoding: identity", "User-Agent: Chrome/83.0.4103.116 Safari/537.36" })
 	@GET("/")
 	Call<Tv> getEpg();
 
