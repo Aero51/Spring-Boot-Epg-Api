f6223483dd1235e00e5396a9004cce11707b4b45
Aero51 <nsrdoc@yahoo.com>
2020-10-26 18:00:27 +0100

    switched back to phoenix rebuild messing around

--- src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
+++ src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
@@ -24,7 +24,7 @@ public interface RetrofitApi {
 	// PostmanRuntime/7.26.5" })
 //	@Headers({ "Content-Type: application/gzip", "User-Agent: PostmanRuntime/7.26.5" })
 	@Headers({ "Content-Type: application/gzip", "User-Agent: PostmanRuntime/7.26.5", "Connection: close" })
-	@GET("/")
+	@GET("/epg.phoenixrebornbuild.com.hr/")
 	Call<Tv> getEpg();
 
 	@GET("proxy?type=http")
