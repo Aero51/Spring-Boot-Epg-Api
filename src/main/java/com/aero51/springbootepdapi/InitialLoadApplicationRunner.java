6d132185c6f899f58c1d7cdaa150bddd13823d8d
Aero51 <nsrdoc@yahoo.com>
2020-10-26 17:20:55 +0100

    switched back to phoenix rebuild messing around

--- src/main/java/com/aero51/springbootepdapi/InitialLoadApplicationRunner.java
+++ src/main/java/com/aero51/springbootepdapi/InitialLoadApplicationRunner.java
@@ -289,20 +289,20 @@ public class InitialLoadApplicationRunner implements ApplicationRunner {
 		channelsRepo.deleteAll();
 		channelsRepo.saveAll(unsortedChannelList);
 		List<OutputChannel> sortedOutputChannelList = new ArrayList<OutputChannel>();
-		// channelsRepo.saveAll(unsortedChannelList);
+		channelsRepo.saveAll(unsortedChannelList);
 		for (int i = 0; i < croChannelList.size(); i++) {
 
-			// sortedOutputChannelList.add(channelsRepo.findByName(croChannelList.get(i)).get(0));
+			sortedOutputChannelList.add(channelsRepo.findByName(croChannelList.get(i)).get(0));
 		}
 
-		// channelsRepo.deleteAll();
+		channelsRepo.deleteAll();
 		for (OutputChannel outputChannel : unsortedChannelList) {
 			if (isSortedExcluded(outputChannel.getName())) {
-				// sortedOutputChannelList.add(outputChannel);
+				sortedOutputChannelList.add(outputChannel);
 			}
 
 		}
-		// channelsRepo.saveAll(sortedOutputChannelList);
+		channelsRepo.saveAll(sortedOutputChannelList);
 
 		System.out.println("number of channels after process unsorted: " + unsortedChannelList.size());
 		// System.out.println("number of channels after process sorted: " +
