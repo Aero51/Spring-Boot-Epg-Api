01f9532a944025784c04996cf12c13f774b9c898
Aero51 <nsrdoc@yahoo.com>
2020-10-28 15:36:24 +0100

    switched back to phoenix rebuild removed proxy

--- src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
+++ src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitApi.java
@@ -23,7 +23,7 @@ public interface RetrofitApi {
 	// @Headers({ "Content-Type: application/gzip", "User-Agent:
 	// PostmanRuntime/7.26.5" })
 //	@Headers({ "Content-Type: application/gzip", "User-Agent: PostmanRuntime/7.26.5" })
-	@Headers({ "User-Agent: PostmanRuntime/7.27.5", "Connection: keep-alive", "Accept-Encoding: gzip" })
+	@Headers({ "Content-Type: application/gzip", "User-Agent: PostmanRuntime/7.27.5" })
 	@GET("/")
 	Call<Tv> getEpg();
 
