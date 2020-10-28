9b54aac7d8cab16b736695adec69e6887c8d4355
Aero51 <nsrdoc@yahoo.com>
2020-10-28 18:38:36 +0100

    switched back to phoenix rebuild removed proxy

--- src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitInstance.java
+++ src/main/java/com/aero51/springbootepdapi/retrofit/RetrofitInstance.java
@@ -40,6 +40,7 @@ public class RetrofitInstance {
 			OkHttpClient okHttpClient = new OkHttpClient.Builder().proxy(proxy).followRedirects(true)
 					.followSslRedirects(true).connectTimeout(20, TimeUnit.SECONDS).readTimeout(20, TimeUnit.SECONDS)
 					.callTimeout(40, TimeUnit.SECONDS).retryOnConnectionFailure(false)
+
 					.addInterceptor(loggingInterceptor).addNetworkInterceptor(new Interceptor() {
 						@NotNull
 						@Override
